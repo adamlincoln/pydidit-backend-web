412dd3719fc93b29b643cc1d2e1778497fcff687
Adam J. Lincoln <adamjlincoln@gmail.com>
2013-12-24 10:51:48 -0500

    Check for non-200 status codes and do something better.
    Make commit() do nothing.

--- src/pydiditbackendweb/__init__.py
+++ src/pydiditbackendweb/__init__.py
@@ -44,7 +44,10 @@ def _send(f, args, kwargs):
         'args': json.dumps(args, default=encode_datetime),
         'kwargs': json.dumps(kwargs, default=encode_datetime),
     }
-    return json.loads(requests.post(base_url, data=data).text, object_hook=decode_datetime)
+    response = requests.post(base_url, data=data)
+    if response.status_code != 200:
+        raise RemoteException(response.status_code, response.text)
+    return json.loads(response.text, object_hook=decode_datetime)
 
 
 function_template = '''def {function_name}(*args, **kwargs):
@@ -67,10 +70,24 @@ for function_name in [
                'link',
                'unlink',
                'flush',
-               'commit',
                'relationship_name',
               ]:
     exec function_template.format(function_name=function_name)
 
+# Special cases
 
+def commit(*args, **kwargs):
+    pass
 
+# End special cases
+
+class RemoteException(Exception):
+    def __init__(self, code, text):
+        self.code = code
+        self.text = text
+
+    def __str__(self):
+        return '{0}: {1}'.format(self.code, self.text)
+
+    def __repr__(self):
+        return str(self)
